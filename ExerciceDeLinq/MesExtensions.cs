d7636647b763e9a48daad04ce412d59be87368e4
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-15 16:43:28 +0200

    Ajoutez des fichiers projet.

---
+++ ExerciceDeLinq/MesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExerciceDeLinq
+{
+    public static class MesExtensions
+    {
+        public static TResult ExecuteScalar<T, TResult>(this T origin, Func<T,TResult> select)
+        {
+            return select(origin);
+        }
+    }
+}
---
+++ ExerciceDeLinq/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LINQDataContext;
+
+namespace ExerciceDeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext dc = new DataContext();
+
+            //var resultat = dc.Students
+            //    .Select(s => new { NomDeFamille = s.Last_Name, DateNaissance = s.BirthDate, s.Login, ResultatAnnuel = s.Year_Result });
+
+            var resultat = from s in dc.Students
+                           select new { NomDeFamille = s.Last_Name, DateNaissance = s.BirthDate, s.Login, ResultatAnnuel = s.Year_Result };
+
+            foreach (var item in resultat) Console.WriteLine($"{item.NomDeFamille} {item.DateNaissance} {item.Login} {item.ResultatAnnuel}");
+
+            #region Console.ReadLine()
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
