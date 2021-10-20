bb2c40189792ccc927652a6e146c29c488b759fc
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-20 10:18:44 +0200

    Add Exos 2.3

--- ExerciceDeLinq/Program.cs
+++ ExerciceDeLinq/Program.cs
@@ -25,17 +25,28 @@ namespace ExerciceDeLinq
             #endregion
             #region Exos2.2
 
-            //var result = dc.Students.Select(student => new { NomComplet = $"{student.Last_Name} {student.First_Name}", student.Student_ID, student.BirthDate });
+            ////var result = dc.Students.Select(student => new { NomComplet = $"{student.Last_Name} {student.First_Name}", student.Student_ID, student.BirthDate });
 
-            var result = from student in dc.Students
-                         select new { NomComplet = $"{student.Last_Name} {student.First_Name}", student.Student_ID, student.BirthDate };
+            //var result = from student in dc.Students
+            //             select new { NomComplet = $"{student.Last_Name} {student.First_Name}", student.Student_ID, student.BirthDate };
 
-            foreach (var stu in result)
-            {
-                Console.WriteLine($"{stu.NomComplet} - {stu.Student_ID} - {stu.BirthDate}");
-            }
+            //foreach (var stu in result)
+            //{
+            //    Console.WriteLine($"{stu.NomComplet} - {stu.Student_ID} - {stu.BirthDate}");
+            //}
             #endregion
 
+            #region Exos2.3
+            //IEnumerable<string> result = dc.Students.Select(student => $"{student.First_Name} | {student.Last_Name} | {student.Student_ID} | {student.BirthDate} | {student.Login} | {student.Section_ID} | {student.Year_Result} | {student.Course_ID}");
+
+            IEnumerable<string> result = from stud in dc.Students
+                                         select $"{stud.First_Name} | {stud.Last_Name} | {stud.Student_ID} | {stud.BirthDate} | {stud.Login} | {stud.Section_ID} | {stud.Year_Result} | {stud.Course_ID}";
+
+            foreach (string stu in result)
+            {
+                Console.WriteLine(stu);
+            }
+            #endregion
             #region Console.ReadLine()
             Console.ReadLine();
             #endregion
